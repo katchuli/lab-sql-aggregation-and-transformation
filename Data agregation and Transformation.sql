ad56aefe4accf9fc6252bd0268090f95d972fd29
Katherine Chulvi <katherinechulvi.da@gmail.com>
2024-01-24 23:11:54 +0100

    done until Challenge 2 1.3

---
+++ Data agregation and Transformation.sql
@@ -0,0 +1,60 @@
+## CHALLENGE 1
+# 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+SELECT MAX(length) AS max_duration, MIN(length) AS min_duration
+FROM sakila.film;
+
+SELECT title, length
+FROM sakila.film
+ORDER BY length DESC;
+
+# 1.2. Express the average movie duration in hours and minutes. Don't use decimals.
+SELECT sec_to_time((time_format((ROUND(AVG(length)),"%H:%i"))))
+FROM sakila.film;
+
+SELECT sec_to_time(ROUND(AVG(length),0)*60) 
+
+# 2.1 Calculate the number of days that the company has been operating. 
+# Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date 
+
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS operating_days
+FROM sakila.rental; 
+
+# 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental.
+--  Return 20 rows of results.
+
+SELECT WEEKDAY(rental_date) AS weekday, MONTH(rental_date) AS month
+FROM sakila.rental limit 20; 
+
+# 3 You need to ensure that customers can easily access information about the movie collection. 
+-- To achieve this, retrieve the film titles and their rental duration. 
+-- If any rental duration value is NULL, replace it with the string 'Not Available'. 
+-- Sort the results of the film title in ascending order.
+
+SELECT title, ifnull(rental_duration, 'Not Available')
+FROM sakila.film
+ORDER BY title ASC;
+
+## CHALLENGE 2
+-- Next, you need to analyze the films in the collection to gain some more insights. 
+-- Using the film table, determine:
+# 1.1 The total number of films that have been released.
+SELECT COUNT(rental_rate) FROM sakila.film;
+# 1.2 The number of films for each rating.
+SELECT COUNT(DISTINCT rating) FROM sakila.film;
+#1.3 The number of films for each rating, sorting the results in descending order of the number of films. 
+-- This will help you to better understand the popularity of different film ratings and adjust purchasing 
+-- decisions accordingly.
+SELECT DISTINCT rental_rate
+FROM sakila.film
+ORDER BY rental_rate DESC;
+
+
+SELECT description  FROM sakila.film;
+
+
+
+
+
+
+
+
